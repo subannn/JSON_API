f8d2ed8b9d0842a20134ad631f48463578f1d1e9
subannn <ibraimov534@gmail.com>
2023-06-08 20:04:38 +0600

    f

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ import (
 
 func main() {
 	var wait time.Duration
-	flag.DurationVar(&wait, "graceful-timeout", time.Second * 15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.DurationVar(&wait, "graceful-timeout", time.Second * 105, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
     flag.Parse()	
 
 	db.OpenDB()
@@ -40,8 +40,11 @@ func main() {
         WriteTimeout: 15 * time.Second,
 		
 	}
-	
-	log.Fatal(server.ListenAndServe())
+	go func() {
+        if err := server.ListenAndServe(); err != nil {
+            log.Println(err)
+        }
+    }()
 
 	c := make(chan os.Signal, 1)
     
